4a3ab6c5679557efbb04decd710aa49afc6b2cac
xtophercoder <xtopherprogrammer@gmail.com>
2024-01-19 14:54:44 +0100

    fetch() update

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-function fetchAdvice() {
-    fetch("https://api.adviceslip.com/advice")
-  .then((res) => res.json())
-  .then((data) => {
-    document.querySelector("#advice").textContent = data.slip.advice;
-    document.querySelector("#slip__id").textContent = `ADVICE
-     # ${data.slip.id}`
-  });
+// function fetchAdvice() {
+//     fetch("https://api.adviceslip.com/advice")
+//   .then((res) => res.json())
+//   .then((data) => {
+//     document.querySelector("#advice").textContent = data.slip.advice;
+//     document.querySelector("#slip__id").textContent = `ADVICE
+//      # ${data.slip.id}`
+//   });
+// }
+
+async function fetchAdvice() {
+  const res = await fetch("https://api.adviceslip.com/advice");
+  const data = await res.json();
+
+  document.querySelector("#advice").textContent = data.slip.advice;
+  document.querySelector("#slip__id").textContet = `ADVICE # ${data.slip.id}`;
 }
